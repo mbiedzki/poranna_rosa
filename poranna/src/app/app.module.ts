a609a6b21c950139d11889adee982434ffd3ce73
Michal Biedzki <mbiedzki@me.com>
2020-05-25 15:02:12 +0200

    zarys 2

--- poranna/src/app/app.module.ts
+++ poranna/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './header/header.component';
-import { PanelTextComponent } from './panel-text/panel-text.component';
+import { MainComponent } from './main/main.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PanelTextComponent
+    MainComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
--- poranna/src/app/main/main.component.spec.ts
+++ poranna/src/app/main/main.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PanelTextComponent } from './panel-text.component';
+import { MainComponent } from './main.component';
 
-describe('PanelTextComponent', () => {
-  let component: PanelTextComponent;
-  let fixture: ComponentFixture<PanelTextComponent>;
+describe('MainComponent', () => {
+  let component: MainComponent;
+  let fixture: ComponentFixture<MainComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PanelTextComponent ]
+      declarations: [ MainComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PanelTextComponent);
+    fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
