cc67e5ce2031bad13e627d5da5b5a7e07a7475d1
Michal Biedzki <mbiedzki@me.com>
2021-04-08 10:35:03 +0200

    v 1.1

--- poranna/src/app/header/header.component.ts
+++ poranna/src/app/header/header.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openMap(): void {
+    console.log('mapa');
+    window.open('https://goo.gl/maps/LcYZAdwVFmUwUu347');
   }
-
 }
